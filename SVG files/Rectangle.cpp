35b487509721e7ca8990353c009f194de74d0448
kyotov1 <kirilyotov@outlook.com>
2022-05-30 01:11:57 +0300

    Rectangle class

--- SVG files/Rectangle.cpp
+++ SVG files/Rectangle.cpp
@@ -1,1 +1,33 @@
 #include "Rectangle.h"
+
+Rectangle::Rectangle(double x, double y, double _width, double _height,
+	const std::string& _fill, double stroke, const std::string& strokeWidth) : Figure(x, y, stroke, strokeWidth) , 
+	width(_width), height(_height), fill(_fill) { }
+
+std::ostream& operator<<(std::ostream& out, const Rectangle& rectangle)
+{
+	out << "circle " << rectangle.points.getX() << " " << rectangle.points.getX() << " " << rectangle.width << " " << rectangle.height << " " << rectangle.fill;
+
+	if (rectangle.strokeWidth > 0 || rectangle.stroke != "none")
+		out << " " << rectangle.strokeWidth << " " << rectangle.stroke;
+
+	return out;
+}
+
+std::ifstream& operator>>(std::ifstream& in, Rectangle& rectangle)
+{
+	return in;
+}
+
+std::ofstream& operator<<(std::ofstream& out, Rectangle& rectangle)
+{
+	out << "<rect x=\"" << rectangle.x << "\" y=\"" << rectangle.y << "\" width=\"" << rectangle.width
+		<< "\" height=\"" << rectangle.height << "\" r=\" fill=\"" << rectangle.fill;
+
+	if (rectangle.strokeWidth > 0 || rectangle.stroke != "none")
+		out << " stroke=\"" << rectangle.stroke << "\"  stroke-width=\"" << rectangle.stroke;
+
+	out << "\" />";
+
+	return out;
+}
--- SVG files/Rectangle.h
+++ SVG files/Rectangle.h
@@ -3,5 +3,16 @@
 class Rectangle :
     public Figure
 {
+    double width;
+    double height;
+    std::string fill;
+
+public:
+  
+    Rectangle(double x, double y, double _width, double _height, const std::string& _fill, double stroke = 0, const std::string& strokeWidth = "none");
+    friend std::ostream& operator << (std::ostream& out, const Rectangle& rectangle);
+   
+    friend std::ifstream& operator >> (std::ifstream& in, Rectangle& rectangle);
+    friend std::ofstream& operator << (std::ofstream& out, Rectangle& rectangle);
 };
 
