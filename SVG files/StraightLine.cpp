fa670ebcb33de845073611e87001e6ec69f9368f
kyotov1 <kirilyotov@outlook.com>
2022-05-30 01:11:35 +0300

    Class StraightLine

--- SVG files/StraightLine.cpp
+++ SVG files/StraightLine.cpp
@@ -1,1 +1,25 @@
 #include "StraightLine.h"
+
+StraightLine::StraightLine(double x, double y, double x2, double y2, double strokeWidth, const std::string& stroke) :
+	Figure(x, y, strokeWidth, stroke), secPoint(x2, y2) { }
+
+std::ostream& operator<<(std::ostream& out, const StraightLine& line)
+{
+	out << "line " << line.points.getX() << " " << line.points.getY() << " " << line.secPoint.getX() << " " << line.secPoint.getY()
+		<< " " << line.strokeWidth << " " << line.stroke;
+	return out;
+}
+
+std::ifstream& operator>>(std::ifstream& in, StraightLine& line)
+{
+	return in;
+}
+
+std::ofstream& operator<<(std::ofstream& out, StraightLine& line)
+{
+	out << "<circle x1=\"" << line.points.getX() << "\" y1=\"" << line.secPoint.getY() << "\" x2=\"" << line.secPoint.getY()
+		<< "\" y2=\"" << line.secPoint.getY() << " stroke=\"" << line.stroke << "\"  stroke-width=\""
+		<< line.stroke << "\" />";
+
+	return out;
+}
--- SVG files/StraightLine.h
+++ SVG files/StraightLine.h
@@ -1,7 +1,16 @@
 #pragma once
 #include "Figure.h"
+
 class StraightLine :
     public Figure
 {
+    Point2D secPoint;
+
+public:
+    StraightLine(double x, double y, double x2, double y2, double strokeWidth , const std::string& stroke );
+    friend std::ostream& operator << (std::ostream& out, const StraightLine& line);
+
+    friend std::ifstream& operator >> (std::ifstream& in, StraightLine& line);
+    friend std::ofstream& operator << (std::ofstream& out, StraightLine& line);
 };
 
