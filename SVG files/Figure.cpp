96299a46bbff10cc3aeafa17a4b93407f211abf8
kyotov1 <kirilyotov@outlook.com>
2022-05-30 01:05:59 +0300

    Constructor created in Figure class

--- SVG files/Figure.cpp
+++ SVG files/Figure.cpp
@@ -1,1 +1,4 @@
 #include "Figure.h"
+
+Figure::Figure(double x, double y, double strokeWidth,const std::string& _stroke) : points(x,y),
+strokeWidth(strokeWidth), stroke(_stroke) { }
--- SVG files/Figure.h
+++ SVG files/Figure.h
@@ -1,7 +1,24 @@
 #pragma once
-class Figure
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include "Point2D.h"
+class Figure 
 {
-    Figure();
+protected:
+    Point2D points;
+
+ 
+    std::string stroke;
+    double strokeWidth;
+public:
+    Figure(double x, double y, double _strokeWidth,const std::string& _stroke);
+    virtual ~Figure() = default;
     
+   /* virtual void create() const = 0;
+    virtual void erase(size_t index) const = 0;
+    virtual void translate(size_t index) const = 0;
+    virtual std::vector<Figure> within (bool (*cmp)(const Figure&) ) const = 0;
+    */
 };
 
