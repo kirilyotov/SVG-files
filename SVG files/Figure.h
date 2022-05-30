95330164f7ddcb50257c9cdf84717f31060fcb36
kyotov1 <kirilyotov@outlook.com>
2022-05-30 23:20:28 +0300

    Removed comments and not used code

--- SVG files/Figure.h
+++ SVG files/Figure.h
@@ -6,19 +6,13 @@
 class Figure 
 {
 protected:
-    Point2D points;
-
- 
+    Point2D points; 
     std::string stroke;
     double strokeWidth;
+
 public:
     Figure(double x, double y, double _strokeWidth,const std::string& _stroke);
     virtual ~Figure() = default;
     
-   /* virtual void create() const = 0;
-    virtual void erase(size_t index) const = 0;
-    virtual void translate(size_t index) const = 0;
-    virtual std::vector<Figure> within (bool (*cmp)(const Figure&) ) const = 0;
-    */
 };
 
