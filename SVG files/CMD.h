677c6ae6331a81c772965e275c8d3fd97e96653c
kyotov1 <kirilyotov@outlook.com>
2022-05-31 01:00:10 +0300

    added new functions

--- SVG files/CMD.h
+++ SVG files/CMD.h
@@ -1,19 +1,30 @@
 #pragma once
 
 #include "Collection.h"
+#include "CommandLineArguments.h"
 
 class CMD
 {
 private:
 	CMD();
 	Collection collection;
-	
 	static const  std::string header;
-	void print();
-	void create();
-	void erase(size_t index);
-	void translate(size_t index);
-	void within();
+	static const std::vector<std::string> integratedCommands;
+
+	void isCorrectCommand(CommandLineArguments& lines);
+
+	void open(std::string file); //0
+	void close();//1
+	void save();//2
+	void saveAs(std::string directory);//3
+	void help();//4
+	void print();//5
+	void exit();//6
+	void create();//7
+	void erase(size_t index);//8
+	void translate(size_t index);//9
+	void within();//10
+
 	void writeToFile();
 	void readFromFile();
 	void createTemporaryFile();
